efaab8872ef1d1df3430e9d5086cd49afca288b9
Matheus Fábio <matheusfabiors@gmail.com>
2025-10-14 02:36:55 -0300

    INIT: Initial learn about Material 3

---
+++ app/src/main/java/com/br/android_material_dio/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.br.android_material_dio
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.br.android_material_dio.databinding.ActivityMainBinding
+
+
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        insertListeners()
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+
+    private fun insertListeners() {
+        binding.cvButtons.setOnClickListener {
+            startActivity(ButtonsActivity.createIntent(this))
+        }
+    }
+}
